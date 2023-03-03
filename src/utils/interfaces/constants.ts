8a95be7e3894dae4515b49f61edc02d2a7a74875
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-03-03 07:51:15 +0700

    update interfaces

--- src/utils/interfaces/constants.ts
+++ src/utils/interfaces/constants.ts
@@ -1,4 +1,25 @@
+import { AxiosHeaders } from "axios";
+import { SweetAlertOptions } from "sweetalert2";
 
 
-export const serverDevURL = process.env.NEXT_PUBLIC_URL
+export const baseUrl = process.env.NEXT_PUBLIC_URL
 export type AlertType = "info" | "danger" | "success" | "warning";
+
+export type ServerResponse = {
+    config: Object | any | undefined,
+    headers: AxiosHeaders,
+    request: XMLHttpRequest | undefined,
+    data: Object | Object[] | undefined,
+    statusText: string,
+    status: number | string;
+}
+
+export const baseAlertStyle: Partial<SweetAlertOptions> = {
+    cancelButtonColor: "#cb1a52",
+    confirmButtonColor: "#8c4dcb",
+}
+
+export const deleteAlertStyle: Partial<SweetAlertOptions> = {
+    confirmButtonColor: "#cb1a52",
+    cancelButtonColor: "#8c4dcb",
+}
