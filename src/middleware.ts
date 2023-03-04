e56ec229d8dfdfc8f6965b119c4d86dfe7c8324c
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-03-04 16:50:15 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,23 @@
-import Cookies from "js-cookie";
 import { NextRequest, NextResponse } from "next/server";
-import { requestRefreshToken } from "./utils/helper/axios-helper";
 import jwt_decode from "jwt-decode";
 import { JWTServer } from "./utils/interfaces/server-props";
 
 export function middleware(request: NextRequest) {
     const pathName = request.nextUrl.pathname;
     let accessToken = request.cookies.get('accessToken')?.value
-    let refreshToken = request.cookies.get('accessToken')?.value
 
-    if (pathName == "/") {
+
+    if (pathName === "/") {
         return NextResponse.redirect(new URL("/auth/login", request.url))
     }
 
-    if (request.nextUrl.pathname.startsWith('/auth')) {
-        let accessToken = request.cookies.get('accessToken')?.value
-        if (accessToken) {
-            const decoded: JWTServer = jwt_decode(accessToken);
-            if (decoded.exp * 1000 > Date.now()) {
-                if (decoded.role == "admin") {
-                    return NextResponse.redirect(new URL("/admin", request.url))
-                } else {
-                    return NextResponse.redirect(new URL("/me", request.url))
-                }
-            }
-        } else if (refreshToken) {
-            requestRefreshToken(refreshToken)
-        } else {
-            return NextResponse.redirect(new URL("/auth/login", request.url))
-        }
+    if (!accessToken) {
+        const response = NextResponse.redirect(new URL("/auth/login", request.url))
+        response.cookies.delete("accessToken")
+        return response
     }
+}
+
+export const config = {
+    matcher: ["/me/:path*", "/admin/:path*", "/"]
 }
