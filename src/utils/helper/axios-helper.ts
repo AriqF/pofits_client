d682a22aa679dea58b5dc44f6ae4ab01de57dd46
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-03-04 16:51:42 +0700

    Update axios-helper.ts

--- src/utils/helper/axios-helper.ts
+++ src/utils/helper/axios-helper.ts
@@ -1,20 +1,12 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import Cookies from 'js-cookie'
+import jwt_decode from "jwt-decode";
 import Router from 'next/router'
-
 
 const client = axios.create({ baseURL: process.env.NEXT_PUBLIC_URL })
 
 export const requestAxios = (options: AxiosRequestConfig) => {
-    // setInterval(()=> {
-    //     refreshToken(Cookies.get('refreshToken'))
-    // },7000)
-    if (!Cookies.get('accessToken')) {
-        requestRefreshToken(Cookies.get('refreshToken'))
-    }
-
     client.defaults.headers.common.Authorization = `Bearer ${Cookies.get('accessToken')}`
-    // console.log(Cookies.get('accessToken'))
     const onSuccess = (response: any) => response;
     const onError = (error: any) => {
         return error
@@ -23,32 +15,8 @@ export const requestAxios = (options: AxiosRequestConfig) => {
     return client(options)
 }
 
-export const requestRefreshToken = (refreshToken: string | undefined) => {
-    if (typeof refreshToken === undefined) throw new Error("Undefined refresh token")
-    console.log('requested to refresh token');
-    axios(`${process.env.NEXT_PUBLIC_URL}/auth/refreshtoken`, {
-        method: 'POST',
-        headers: {
-            Authorization: 'Bearer ' + refreshToken
-        }
-    })
-        .then(
-            res => {
-                const oneHundredMinutes = new Date(new Date().getTime() + 100 * 60 * 1000);
-                Cookies.set('accessToken', res.data.accessToken, { expires: oneHundredMinutes })
-                Router.reload()
-                // return res.data.accessToken
-            }
-        )
-        .catch(
-            err => {
-                console.log(err)
-                if (err.response.status == 401) {
-                    //Session habis
-                    Cookies.remove('accessToken')
-                    Cookies.remove('refreshToken')
-                    return Router.push('/auth/login')
-                }
-            }
-        )
-}
+
+export const logoutHandler = () => {
+    Cookies.remove("accessToken");
+    Router.replace("/auth/login");
+};
