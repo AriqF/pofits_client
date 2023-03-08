05bdb126dcd55eaab66f0fe0d883f315cffe3380
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-03-08 11:01:25 +0700

    Update menu-option.tsx

--- src/components/layouts/user/settings/menu-option.tsx
+++ src/components/layouts/user/settings/menu-option.tsx
@@ -5,13 +5,17 @@ interface Props {
   linkTo?: string;
   children: ReactNode;
   onClick?: MouseEventHandler<HTMLAnchorElement> | undefined;
+  className?: string;
 }
 
 export default function MenuOptionItem(props: Props) {
   return (
     <a
       onClick={props.onClick}
-      className="bg-white inline-flex border-b border-gray-200 px-3 py-3 text-base font-semibold capitalize cur"
+      className={
+        "bg-white inline-flex border-b border-gray-200 px-3 py-3 text-base font-semibold capitalize " +
+        props.className
+      }
       href={props.linkTo}>
       {props.children}
     </a>
