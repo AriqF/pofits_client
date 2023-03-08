ec592bdfeb072264f86a6c8db833519944abd233
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-03-08 11:04:21 +0700

    Update base-list.tsx

--- src/components/tools/list/base-list.tsx
+++ src/components/tools/list/base-list.tsx
@@ -4,18 +4,31 @@ interface Props {
   header: string;
   subHeader?: string;
   shortDesc?: string;
+  className?: string;
 }
 
 export default function BaseList(props: Props) {
   return (
     <a
       href={props.baseUrl + "/" + props.id}
-      className="grid-flow-row grid-cols-1 hover:bg-hovwhite cursor-pointer drop-shadow-md px-3 py-4">
-      <h5 className="text-lg capitalize font-base">
+      className={
+        "group grid-flow-row grid-cols-1 hover:border hover:bg-palepurple hover:text-white rounded-md cursor-pointer drop-shadow-md px-3 py-4  " +
+        " " +
+        props.className
+      }>
+      <h5 className="text-lg capitalize font-base group-hover:text-white text-gray-900">
         {props.header}{" "}
-        {props.subHeader ? <span className="text-gray-400 text-sm">- {props.subHeader}</span> : ""}
+        {props.subHeader ? (
+          <span className="text-sm text-gray-500 group-hover:text-white/[0.7]">
+            - {props.subHeader}
+          </span>
+        ) : (
+          ""
+        )}
       </h5>
-      <span className="text-base text-gray-600">{props.shortDesc}</span>
+      <span className="text-base group-hover:text-white/[0.9] text-gray-700 ">
+        {props.shortDesc}
+      </span>
     </a>
   );
 }
