8d2ef0a2d120c581cf45b18734f4a8e342898931
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-03-08 11:01:00 +0700

    change auth method

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -9,9 +9,13 @@ import { regEmail } from "@/utils/interfaces/regex";
 import AuthLayout from "@/components/layouts/auth";
 import Alert from "@/components/tools/alerts/alert";
 import { baseUrl } from "@/utils/interfaces/constants";
+import { JWTServer } from "@/utils/interfaces/server-props";
+import jwt_decode from "jwt-decode";
+import { NextResponse } from "next/server";
 interface FormState {
   email: string;
   password: string;
+  isKeepSignedIn: boolean;
 }
 
 export default function Login() {
@@ -21,6 +25,7 @@ export default function Login() {
   const [values, setValues] = React.useState<FormState>({
     email: "",
     password: "",
+    isKeepSignedIn: false,
   });
 
   const ref = useRef<any>();
@@ -37,25 +42,27 @@ export default function Login() {
     setValues({ ...values, [prop]: event.target.value });
   };
 
-  const onSubmit: SubmitHandler<FormState> = async (data: any) => {
+  const onSubmit: SubmitHandler<FormState> = async (data: FormState) => {
     SetErrMessage("");
     await axios(`${baseUrl}/auth/login`, {
       method: "POST",
       data: {
         email: data.email,
         password: data.password,
+        isKeepSignedIn: data.isKeepSignedIn,
       },
     })
       .then((res) => {
         SetIsSuccess(res.data.message);
-        //100 minutes session for accesstoken
-        const sessionTime = new Date(new Date().getTime() + 100 * 60 * 1000);
+        const token: string = res.data.accessToken;
+        const decoded: JWTServer = jwt_decode(token);
         cookies.set("accessToken", res.data.accessToken, {
-          expires: sessionTime,
+          expires: decoded.exp,
         });
-        cookies.set("refreshToken", res.data.refreshToken, { expires: 6 });
+        console.log(decoded.exp);
 
         res.data.role === "admin" ? router.push("/admin") : router.push("/me");
+        // return NextResponse.next();
       })
       .catch((error) => {
         SetIsServerError(true);
@@ -115,6 +122,19 @@ export default function Login() {
               })}
             />
           </div>
+          <div>
+            <div className="flex items-center">
+              <input
+                id="keepSigned"
+                type="checkbox"
+                {...register("isKeepSignedIn")}
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 cursor-pointer"
+              />
+              <label htmlFor="keepSigned" className="ml-2 text-sm font-medium text-gray-900">
+                Biarkan saya tetap masuk
+              </label>
+            </div>
+          </div>
         </div>
         <div id="login-button" className="md:w-1/2 flex flex-col md:mx-auto space-y-2">
           <div id="auth-message">{errMessage && <Alert text={errMessage} type="danger" />}</div>
@@ -126,7 +146,9 @@ export default function Login() {
           </button>
         </div>
         <div className="grid grid-cols-1 text-center text-md space-y-2">
-          <a className="text-blue hover:text-hovblue font-bold" href="/auth/forgot-password">
+          <a
+            className="text-blue hover:text-hovblue font-bold w-fit m-auto"
+            href="/auth/forgot-password">
             Lupa kata sandi
           </a>
           <p>
