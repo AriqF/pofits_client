a5c9b7bfc463880ca6a204af0ec6d89cafbbc1ee
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-03-08 10:59:11 +0700

    fix middleware and routing

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,18 +6,43 @@ export function middleware(request: NextRequest) {
     const pathName = request.nextUrl.pathname;
     let accessToken = request.cookies.get('accessToken')?.value
 
-
+    //index middleware
     if (pathName === "/") {
-        return NextResponse.redirect(new URL("/auth/login", request.url))
+        return NextResponse.rewrite(new URL("/auth/login", request.url))
     }
 
-    if (!accessToken) {
-        const response = NextResponse.redirect(new URL("/auth/login", request.url))
-        response.cookies.delete("accessToken")
-        return response
+    //Auth middleware
+    if (request.nextUrl.pathname.startsWith('/auth')) {
+        if (accessToken) {
+            const decoded: JWTServer = jwt_decode(accessToken);
+            if (decoded.role === "admin") {
+                return NextResponse.redirect(new URL("/admin", request.url))
+            } else if (decoded.role === "user") {
+                return NextResponse.redirect(new URL("/me", request.url))
+            } else {
+                return NextResponse.redirect(new URL("/auth/login", request.url))
+            }
+        }
+    }
+
+    //Protected routes middleware
+    if (request.nextUrl.pathname.startsWith("/me") || request.nextUrl.pathname.startsWith("/admin")) {
+        if (accessToken) {
+            const decoded: JWTServer = jwt_decode(accessToken);
+            if (request.nextUrl.pathname.startsWith("/admin") && decoded.role !== "admin") {
+                return NextResponse.redirect(new URL("/me", request.url))
+            }
+            if (request.nextUrl.pathname.startsWith("/me") && decoded.role !== "user") {
+                return NextResponse.redirect(new URL("/admin", request.url))
+            }
+        } else {
+            const response = NextResponse.redirect(new URL("/auth/login", request.url))
+            response.cookies.delete("accessToken")
+            return response
+        }
     }
 }
 
 export const config = {
-    matcher: ["/me/:path*", "/admin/:path*", "/"]
+    matcher: ["/me/:path*", "/admin/:path*", "/auth/:path*", "/"]
 }
