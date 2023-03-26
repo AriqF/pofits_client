653ae853d5bbb8fb75ea4fe05326929260800bc9
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-03-26 18:29:51 +0700

    create user path enum

---
+++ src/utils/global/route-path.ts
@@ -0,0 +1,23 @@
+
+
+export enum UserPath {
+    HOME = "/me/",
+    SETTINGS = "/me/settings/",
+    PROFILE = "/me/settings/profile",
+    WALLETS = "/me/settings/wallets/",
+    WALLETS_ADD = "/me/settings/wallets/add",
+    WALLETS_EDIT = "/me/settings/wallets/",
+    WALLETS_TRANSFER = "/me/settings/wallets/transfer-funds/",
+    INCOME_CATEGORY = "/me/settings/income-category/",
+    INCOME_CATEGORY_ADD = "/me/settings/income-category/add/",
+    INCOME_CATEGORY_EDIT = "/me/settings/income-category/",
+    EXPENSE_CATEGORY = "/me/settings/expense-category/",
+    EXPENSE_CATEGORY_ADD = "/me/settings/expense-category/add/",
+    EXPENSE_CATEGORY_EDIT = "/me/settings/expense-category/",
+    BUDGET = "/me/budget/",
+    BUDGET_ADD = "/me/budget/add/",
+    ESTIMATION = "/me/income-estimation/",
+    ESTIMATION_ADD = "/me/income-estimation/add",
+    FINANCE_GOAL = "/me/finance-goals/"
+
+} 
