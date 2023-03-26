0d47a39af2914d5bf8be0b4e3bb30a2d72d0d67b
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-03-26 18:32:21 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import "@/styles/globals.css";
+import "flowbite";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Script from "next/script";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,9 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Pofits - Personal Finance</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <Script src="https://kit.fontawesome.com/babb4f3fd7.js" crossOrigin="anonymous"></Script>
       </Head>
       <Component {...pageProps} />
+      {/* <Script src="https://kit.fontawesome.com/babb4f3fd7.js" crossOrigin="anonymous"></Script> */}
     </>
   );
 }
