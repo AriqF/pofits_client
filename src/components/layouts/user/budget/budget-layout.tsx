77bdc31b25df9cca55fd3ff52c4dd8f71288e3bf
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-03-30 10:57:31 +0700

    Update budget-layout.tsx

--- src/components/layouts/user/budget/budget-layout.tsx
+++ src/components/layouts/user/budget/budget-layout.tsx
@@ -13,17 +13,17 @@ interface Props {
 export default function BudgetPageLayout(props: Props) {
   return (
     <UserBaseLayout>
+      <header className="p-0 min-h-fit inline-flex flex-col gap-y-4 mb-4 md:mb-2">
+        <a
+          href={props.backTo}
+          className="text-blue hover:text-hovblue font-semibold inline-flex cursor-pointer">
+          <MdChevronLeft className="my-auto text-2xl" /> <p>Kembali</p>
+        </a>
+      </header>
       <section
         className={
           "min-h-screen lg:mb-0 lg:grid lg:grid-cols-3 lg:grid-flow-col lg:gap-x-10 space-y-4 md:space-y-0 "
         }>
-        <header className="p-0 min-h-fit flex flex-col gap-y-4 md:hidden">
-          <a
-            href={props.backTo}
-            className="text-blue hover:text-hovblue font-semibold inline-flex cursor-pointer">
-            <MdChevronLeft className="my-auto text-2xl" /> <p>Kembali</p>
-          </a>
-        </header>
         {props.children}
       </section>
     </UserBaseLayout>
