ee3a8fdcd04ce4ca0f8f77a013048d58ef04ed31
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-03-31 22:11:05 +0700

    revamp income and expense category

--- src/pages/me/settings/income-category/[id].tsx
+++ src/pages/me/settings/income-category/[id].tsx
@@ -1,38 +1,44 @@
 import UserSettingsLayout from "@/components/layouts/user/settings";
-import UserSettingsHeader from "@/components/layouts/user/settings/header-settings";
 import Alert from "@/components/tools/alerts/alert";
 import FormHelper from "@/components/tools/alerts/form-helper";
 import DefaultButton from "@/components/tools/button";
-import UserSettingContentBox from "@/components/tools/container/user-settings-content-box";
 import UserSettingsDeleteBox from "@/components/tools/container/user-settings-delete-box";
+import InputForm from "@/components/tools/form/input-form";
+import { UserPath } from "@/utils/global/route-path";
+import { iconOpt, incomeTypeOpt } from "@/utils/global/select-options";
 import {
   baseAlertStyle,
   baseFormStyle,
   deleteAlertStyle,
   deleteButtonStyle,
+  selectFormStyle,
 } from "@/utils/global/style";
 import { CustomAlert } from "@/utils/helper";
 import { requestAxios } from "@/utils/helper/axios-helper";
 import { baseUrl } from "@/utils/interfaces/constants";
 import { ServerMessage } from "@/utils/interfaces/response-message";
-import { ExpenseCategory, IncomeCategory } from "@/utils/interfaces/server-props";
+import { FormIncomeCategory, IncomeCategory } from "@/utils/interfaces/server-props";
 import { useRouter } from "next/router";
-import { FormHTMLAttributes, LegacyRef, useEffect, useRef, useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useEffect, useRef, useState } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import ReactSelect from "react-select";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
+import Image from "next/image";
 
 export default function IncomeCategoryDetails() {
   const [isGlobal, setIsGlobal] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
   const [isServerError, setIsServerError] = useState(false);
   const [errMessage, setErrMessage] = useState("");
+
   const router = useRouter();
   const swal = withReactContent(Swal);
   const dataId = router.query.id;
+  const ref = useRef<any>();
 
   const getCategoryDetail = async () => {
-    await requestAxios({
+    const res = await requestAxios({
       url: baseUrl + "/income-category/" + dataId,
       method: "GET",
     })
@@ -40,13 +46,22 @@ export default function IncomeCategoryDetails() {
         setValue("id", res.data.id);
         setValue("title", res.data.title);
         setValue("description", res.data.description);
-        setValue("income_type", res.data.income_type);
         setValue("created_at", res.data.created_at);
         setValue("updated_at", res.data.updated_at);
-        setIsGlobal(res.data.isGlobal);
+        setValue("income_type", {
+          value: res.data.income_type,
+          label: res.data.income_type == "aktif" ? "Aktif" : "Pasif",
+        });
+        setValue("icon", {
+          value: res.data.icon,
+        });
       })
       .catch((err) => {
-        return CustomAlert({ linkToConfirm: "/me/settings/income-category" });
+        return CustomAlert({
+          title: "Terjadi kesalahan dalam pengambilan data",
+          text: err,
+          linkToConfirm: UserPath.INCOME_CATEGORY,
+        });
       });
   };
 
@@ -54,59 +69,19 @@ export default function IncomeCategoryDetails() {
     register,
     handleSubmit,
     setValue,
+    control,
     formState: { errors, isSubmitting },
-  } = useForm<IncomeCategory>();
+  } = useForm<FormIncomeCategory>();
 
-  const deleteCategory = () => {
-    swal
-      .fire({
-        title: "Hapus kategori?",
-        icon: "warning",
-        showCancelButton: true,
-        showConfirmButton: true,
-        confirmButtonText: "Hapus kategori",
-        cancelButtonText: "Batal",
-        ...deleteAlertStyle,
-      })
-      .then(async (res) => {
-        if (res.isConfirmed) {
-          await requestAxios({
-            url: baseUrl + "/income-category/soft-delete/" + dataId,
-            method: "DELETE",
-          })
-            .then(() => {
-              return swal
-                .fire({
-                  title: "Kategori berhasil dihapus",
-                  icon: "success",
-                  showConfirmButton: true,
-                  ...baseAlertStyle,
-                })
-                .then((res) => {
-                  if (res.isConfirmed) router.push("/me/settings/income-category");
-                });
-            })
-            .catch((err) => {
-              return swal.fire({
-                title: "Kategori gagal dihapus",
-                text: err,
-                icon: "error",
-                showConfirmButton: true,
-                ...deleteAlertStyle,
-              });
-            });
-        }
-      });
-  };
-
-  const onSubmit: SubmitHandler<IncomeCategory> = async (data: IncomeCategory) => {
+  const onSubmit: SubmitHandler<FormIncomeCategory> = async (data: FormIncomeCategory) => {
     setErrMessage("");
     await requestAxios({
       url: baseUrl + "/income-category/update/" + dataId,
       method: "PATCH",
       data: {
         title: data.title,
-        income_type: data.income_type,
+        income_type: data.income_type.value,
+        icon: data.icon.value,
         description: data.description,
       },
     })
@@ -137,38 +112,96 @@ export default function IncomeCategoryDetails() {
   }, [router.isReady]);
 
   return (
-    <UserSettingsLayout>
-      <UserSettingsHeader backTo="/me/settings/income-category">
+    <UserSettingsLayout backTo={UserPath.INCOME_CATEGORY}>
+      <section className="flex flex-col gap-y-4 col-span-2">
         <div className="grid grid-cols-6 max-[350px]:flex flex-col max-[350px]:gap-y-2 ">
           <h3 className="text-2xl font-bold col-span-4">Ubah kategori</h3>
         </div>
-      </UserSettingsHeader>
-      <UserSettingContentBox>
-        <form className="flex" onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} ref={ref}>
           <div id="edit-incomecat-form" className="flex flex-col gap-y-5 w-full">
-            <div>
-              <label htmlFor="income-type" className="block mb-2 text-md font-medium text-gray-900">
-                Kategori Pemasukan
-              </label>
-              <select
-                {...register("income_type", { required: "Jenis pemasukan perlu diisi" })}
-                id="income-type"
-                disabled={isGlobal ? true : false}
-                className={
-                  baseFormStyle +
-                  (errors.income_type ? "border-errorRed focus:border-errorRed" : "")
-                }>
-                <option value={"aktif"}>Aktif</option>
-                <option value={"pasif"}>Pasif</option>
-              </select>
-              {errors.income_type && (
-                <FormHelper textColor="danger" text={errors.income_type?.message} />
-              )}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-3 gap-y-5">
+              <InputForm
+                label={"Jenis Pemasukan"}
+                id={"income-type-select"}
+                errors={errors.income_type?.message}>
+                <Controller
+                  control={control}
+                  {...register("income_type", {
+                    required: "Jenis pemasukan perlu diisi",
+                  })}
+                  render={({ field }) => {
+                    return (
+                      <ReactSelect
+                        styles={{
+                          control: (baseStyles, state) => ({
+                            borderColor: errors.income_type
+                              ? "border-errorRed"
+                              : "focus:border-errorRed",
+                          }),
+                        }}
+                        classNames={{
+                          control: (state) =>
+                            selectFormStyle +
+                            (errors.income_type ? " border-errorRed focus:border-errorRed" : ""),
+                        }}
+                        options={incomeTypeOpt}
+                        value={field.value}
+                        placeholder="Pilih kategori"
+                        className={
+                          errors.income_type ? "border-errorRed focus:border-errorRed" : ""
+                        }
+                        onChange={field.onChange}
+                      />
+                    );
+                  }}
+                />
+              </InputForm>
+              <InputForm
+                label={"Pilih ikon kategori"}
+                id={"income-icon-select"}
+                errors={errors.icon?.message}>
+                <Controller
+                  control={control}
+                  {...register("icon", {
+                    required: "Ikon pemasukan perlu dipilih",
+                  })}
+                  render={({ field }) => {
+                    return (
+                      <ReactSelect
+                        styles={{
+                          control: (baseStyles, state) => ({
+                            borderColor: errors.icon ? "border-errorRed" : "focus:border-errorRed",
+                          }),
+                        }}
+                        classNames={{
+                          control: (state) =>
+                            selectFormStyle +
+                            (errors.icon ? " border-errorRed focus:border-errorRed" : ""),
+                        }}
+                        placeholder="Pilih ikon"
+                        className={errors.icon ? "border-errorRed focus:border-errorRed" : ""}
+                        value={field.value}
+                        options={iconOpt}
+                        onChange={field.onChange}
+                        formatOptionLabel={(item) => (
+                          <div className="inline-flex space-x-3 my-auto">
+                            <Image
+                              src={`/assets/icons/svg/${item.value}.svg`}
+                              alt={`icon-${item.value}`}
+                              width={25}
+                              height={25}
+                              className="my-auto"
+                            />
+                            <p className="my-auto capitalize">{item.value}</p>
+                          </div>
+                        )}
+                      />
+                    );
+                  }}
+                />
+              </InputForm>
             </div>
-            <div>
-              <label htmlFor="title" className="block mb-2 text-md font-medium text-gray-900">
-                Nama Kategori
-              </label>
+            <InputForm label={"Nama Kategori"} id={"category-name"} errors={errors.title?.message}>
               <input
                 disabled={isGlobal ? true : false}
                 {...register("title", { required: "Nama pemasukan harus diisi" })}
@@ -179,12 +212,11 @@ export default function IncomeCategoryDetails() {
                   baseFormStyle + (errors.title ? "border-errorRed focus:border-errorRed" : "")
                 }
               />
-              {errors.title && <FormHelper textColor="danger" text={errors.title?.message} />}
-            </div>
-            <div>
-              <label htmlFor="description" className="block mb-2 text-md font-medium text-gray-900">
-                Deskripsi {"(Optional)"}
-              </label>
+            </InputForm>
+            <InputForm
+              label="Deskripsi (optional)"
+              errors={errors.description?.message}
+              id={"description"}>
               <textarea
                 disabled={isGlobal ? true : false}
                 {...register("description")}
@@ -197,25 +229,14 @@ export default function IncomeCategoryDetails() {
                   (errors.description ? "border-errorRed focus:border-errorRed" : "")
                 }
               />
-              {errors.description && (
-                <FormHelper textColor="danger" text={errors.description?.message} />
-              )}
-            </div>
+            </InputForm>
             <div id="add-incomecat-button" className="md:w-1/2 flex flex-col md:mx-auto space-y-2">
               <div id="auth-message">{errMessage && <Alert text={errMessage} type="danger" />}</div>
               <DefaultButton text="Simpan" color="default" type="submit" className="md:w-1/2" />
             </div>
           </div>
         </form>
-      </UserSettingContentBox>
-      <UserSettingsDeleteBox
-        headerText="Hapus kategori"
-        headerDesc="Kamu sudah tidak bisa menggunakan kategori ini untuk menambah kategori pada transaksi mu selanjutnya">
-        <button onClick={deleteCategory} title="Hapus data" className={deleteButtonStyle}>
-          {/* <BiTrash className="text-2xl m-auto" /> */}
-          Hapus Data
-        </button>
-      </UserSettingsDeleteBox>
+      </section>
     </UserSettingsLayout>
   );
 }
