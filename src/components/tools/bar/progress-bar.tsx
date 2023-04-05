e69dd3aae7dae411515b7d4df40669b892379a89
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-04-05 13:44:16 +0700

    Update progress-bar.tsx

--- src/components/tools/bar/progress-bar.tsx
+++ src/components/tools/bar/progress-bar.tsx
@@ -8,11 +8,17 @@ interface Props {
 
 export default function ProgressBar(props: Props) {
   const { bgColor, textColor, percentage } = props;
+  let percentageNum: number = Number(percentage);
+  if (percentageNum > 100) {
+    percentageNum = 100;
+  }
 
   return (
     <div className="text-center">
       <div className="w-full bg-gray-200 rounded-full h-2.5">
-        <div className={bgColor + " h-2.5 rounded-full"} style={{ width: `${percentage}%` }}></div>
+        <div
+          className={bgColor + " h-2.5 rounded-full"}
+          style={{ width: `${percentageNum}%` }}></div>
       </div>
     </div>
   );
