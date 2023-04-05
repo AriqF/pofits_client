20396a852558b502bf0815734ac5eb67fa420b0e
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-04-05 13:45:44 +0700

    Update [id].tsx

--- src/pages/me/budget/edit/[id].tsx
+++ src/pages/me/budget/edit/[id].tsx
@@ -2,25 +2,30 @@ import BudgetPageLayout from "@/components/layouts/user/budget/budget-layout";
 import Container from "@/components/tools/container";
 import InputForm from "@/components/tools/form/input-form";
 import { UserPath } from "@/utils/global/route-path";
-import { formStyle, selectFormStyle } from "@/utils/global/style";
+import { baseAlertStyle, formStyle, selectFormStyle } from "@/utils/global/style";
 import { CustomAlert, numFormatter } from "@/utils/helper";
 import { requestAxios } from "@/utils/helper/axios-helper";
 import { baseUrl } from "@/utils/interfaces/constants";
-import { BudgetData } from "@/utils/interfaces/server-props";
+import { AddBudgetData, BudgetData } from "@/utils/interfaces/server-props";
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import ReactSelect from "react-select";
 import Image from "next/image";
 import DefaultButton from "@/components/tools/button";
 import Alert from "@/components/tools/alerts/alert";
 import moment from "moment";
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
 
 export default function EditBudgetAllocation() {
   const [budgetDate, setBudgetDate] = useState(new Date());
+  const [category, setCategory] = useState("");
+  const [icon, setIcon] = useState("");
   const router = useRouter();
   const dataId = router.query.id;
   const ref = useRef<any>();
+  const swal = withReactContent(Swal);
 
   const {
     register,
@@ -28,7 +33,27 @@ export default function EditBudgetAllocation() {
     setValue,
     control,
     formState: { errors, isSubmitting },
-  } = useForm<BudgetData>();
+  } = useForm<AddBudgetData>();
+
+  const onSubmit: SubmitHandler<AddBudgetData> = async (data: AddBudgetData) => {
+    await requestAxios({
+      url: baseUrl + "/budget/edit/" + dataId,
+      method: "PATCH",
+      data: {
+        amount: parseInt(data.amount.replace(/\./g, "")),
+      },
+    }).then((res) => {
+      swal
+        .fire({
+          title: `Anggaran ${category} berhasil dirubah`,
+          icon: "success",
+          ...baseAlertStyle,
+        })
+        .then((res) => {
+          if (res.isConfirmed) router.push(UserPath.BUDGET);
+        });
+    });
+  };
 
   const getData = async () => {
     await requestAxios({
@@ -36,12 +61,13 @@ export default function EditBudgetAllocation() {
       method: "GET",
     })
       .then((res) => {
-        setValue("category", res.data.category);
         setValue("amount", res.data.amount);
         setBudgetDate(res.data.start_date);
+        setCategory(res.data.category.title);
+        setIcon(res.data.category.icon);
       })
       .catch((err) => {
-        return CustomAlert({ linkToConfirm: "/me/settings/expense-category" });
+        return CustomAlert({ linkToConfirm: UserPath.BUDGET });
       });
   };
 
@@ -56,58 +82,26 @@ export default function EditBudgetAllocation() {
       <section className="flex flex-col col-span-2">
         <Container className="w-full p-1 md:p-6">
           <div className="mb-3 space-y-4">
-            <h3 className="text-2xl font-semibold">Edit Alokasi Anggaran</h3>
-            <hr className="border-b border-2 border-gray-300 w-full md:w-6/12" />
-            <Alert
-              text={`Anggaran Bulan ${moment(budgetDate).format("MMMM YYYY")}`}
-              type="info"
-              className="w-full md:w-[40%] opacity-90"
-            />
-          </div>
-          <form id="edit-budget-form">
-            <div className="flex flex-col gap-y-5 w-full">
-              <InputForm label="Kategori" id="category-select" errors={errors.category?.message}>
-                <Controller
-                  {...register("category", { required: "Kategori perlu diisi" })}
-                  control={control}
-                  render={({ field }) => {
-                    return (
-                      <ReactSelect
-                        styles={{
-                          control: (baseStyles, state) => ({
-                            // ...baseStyles,
-                            borderColor: errors.category
-                              ? "border-errorRed"
-                              : "focus:border-errorRed",
-                          }),
-                        }}
-                        classNames={{
-                          control: (state) =>
-                            selectFormStyle +
-                            (errors.category ? " border-errorRed focus:border-errorRed" : ""),
-                        }}
-                        placeholder="Pilih kategori"
-                        className={errors.category ? "border-errorRed focus:border-errorRed" : ""}
-                        value={field.value}
-                        isDisabled
-                        onChange={field.onChange}
-                        formatOptionLabel={(item) => (
-                          <div className="inline-flex space-x-3 my-auto">
-                            <Image
-                              src={`/assets/icons/svg/${item.icon}.svg`}
-                              alt="category-icon"
-                              width={30}
-                              height={30}
-                              className="my-auto"
-                            />
-                            <p className="my-auto">{item.title}</p>
-                          </div>
-                        )}
-                      />
-                    );
-                  }}
+            <div className="inline-flex gap-4">
+              <div className="rounded-full p-2 bg-gray-300 my-auto">
+                <Image
+                  src={`/assets/icons/svg/${icon}.svg`}
+                  alt="category-icon"
+                  width={50}
+                  height={50}
+                  className="my-auto"
                 />
-              </InputForm>
+              </div>
+              <div className="my-auto">
+                <h3 className="text-xl font-semibold">Edit Alokasi Anggaran {category}</h3>
+                <h5 className="text-base text-mute capitalize">
+                  {moment(budgetDate).format("MMMM YYYY")}
+                </h5>
+              </div>
+            </div>
+          </div>
+          <form id="edit-budget-form" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-y-5 w-full md:w-[75%]">
               <InputForm label="Berapa anggaran kamu" id="amount" errors={errors.amount?.message}>
                 <div className="flex">
                   <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md ">
@@ -136,7 +130,6 @@ export default function EditBudgetAllocation() {
                   />
                 </div>
               </InputForm>
-
               <DefaultButton
                 type={"submit"}
                 text={"Simpan"}
