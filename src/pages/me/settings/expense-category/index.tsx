6a175a23934c240462b782bc49642e53316b0c7a
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-04-14 17:29:23 +0700

    add alert if data not found

--- src/pages/me/settings/expense-category/index.tsx
+++ src/pages/me/settings/expense-category/index.tsx
@@ -1,5 +1,6 @@
 import UserSettingsLayout from "@/components/layouts/user/settings";
 import UserSettingsHeader from "@/components/layouts/user/settings/header-settings";
+import NotFoundImage from "@/components/tools/alerts/notfound-image";
 import LinkButton from "@/components/tools/button/link-button";
 import UserSettingContentBox from "@/components/tools/container/user-settings-content-box";
 import BaseList from "@/components/tools/list/base-list";
@@ -100,26 +101,30 @@ export default function UserExpenseCategory() {
         </div>
 
         <section id="exp-category-list" className="flex flex-col">
-          {categories.map((category, index) => (
-            <DataList
-              key={index}
-              dataPath={UserPath.EXPENSE_CATEGORY}
-              icon={category.icon}
-              title={category.title}
-              dataId={category.id}>
-              <a
-                href={UserPath.EXPENSE_CATEGORY_EDIT + category.id}
-                className="rounded-full p-2 bg-gray-500 my-auto  hover:bg-gray-400 transition-all duration-200">
-                <MdEditNote className="text-lg my-auto" />
-              </a>
-              <a
-                href="#"
-                onClick={() => deleteCategory(category.id)}
-                className="cursor-pointer rounded-full p-2 bg-gray-500 hover:bg-gray-400 my-auto  transition-all duration-200">
-                <MdDelete className="text-lg my-auto" />
-              </a>
-            </DataList>
-          ))}
+          {categories.length > 0 ? (
+            categories.map((category, index) => (
+              <DataList
+                key={index}
+                dataPath={UserPath.EXPENSE_CATEGORY}
+                icon={category.icon}
+                title={category.title}
+                dataId={category.id}>
+                <a
+                  href={UserPath.EXPENSE_CATEGORY_EDIT + category.id}
+                  className="rounded-full p-2 bg-gray-500 my-auto  hover:bg-gray-400 transition-all duration-200">
+                  <MdEditNote className="text-lg my-auto" />
+                </a>
+                <a
+                  href="#"
+                  onClick={() => deleteCategory(category.id)}
+                  className="cursor-pointer rounded-full p-2 bg-gray-500 hover:bg-gray-400 my-auto  transition-all duration-200">
+                  <MdDelete className="text-lg my-auto" />
+                </a>
+              </DataList>
+            ))
+          ) : (
+            <NotFoundImage text="Belum ada data" imageWidth={200} imageHeight={200} />
+          )}
         </section>
       </section>
     </UserSettingsLayout>
