7fe4715534d498af554833d18a4c787dd79b2654
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-04-14 10:34:38 +0700

    Update style.ts

--- src/utils/global/style.ts
+++ src/utils/global/style.ts
@@ -11,11 +11,11 @@ export const deleteAlertStyle: Partial<SweetAlertOptions> = {
 }
 
 export const defaultButtonStyle: string = " inline-flex place-content-center text-center font-semibold focus:ring-1 focus:outline-none " +
-    "rounded-md text-md px-2 py-3 md:py-2.5 w-full m-auto transition-color duration-200 "
+    "rounded-md text-md px-2 py-3 md:py-2.5 w-full m-auto transition-color duration-200 select-none "
 
 export const deleteButtonStyle: string = "col-span-1 col-start-4 col-end-4 ml-auto my-auto text-white bg-errorRed hover:bg-hovErrorRed focus:ring-hovErrorRed text-center " +
     "font-semibold focus:ring-1 focus:outline-none rounded-md text-sm h-1/4 py-3 w-1/4 md:w-3/4 lg:w-1/4 max-[350px]:w-1/4 max-[350px]:mr-auto " +
-    "max-[350px]:ml-0";
+    "max-[350px]:ml-0 select-none";
 
 export const formStyle: string = "bg-white border border-gray-300 text-gray-900 text-md lock w-full p-2.5 hover:border-blue "
 export const baseFormStyle: string = formStyle + " rounded-md "
