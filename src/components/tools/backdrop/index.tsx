3a91cb9c49a177f18db8f374b3e748f10a1c6904
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-05-27 23:12:14 +0700

    create backdrop component

---
+++ src/components/tools/backdrop/index.tsx
@@ -0,0 +1,7 @@
+export default function Backdrop() {
+  return (
+    <div
+      drawer-backdrop=""
+      className="bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-30"></div>
+  );
+}
